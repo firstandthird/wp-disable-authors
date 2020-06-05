74ad15c3134a626feefb999251e22821c146829d
Troy Whiteley <24781+dawnerd@users.noreply.github.com>
2020-06-05 19:46:11 +0000

    Filter out author name on oembed

--- inc/rename-author.php
+++ inc/rename-author.php
@@ -7,9 +7,20 @@ function disableauthors_filter_author($name) {
   return $disableauthorsAuthorName;
 }
 
+function disableauthors_filter_oembed($data) {
+  global $disableauthorsAuthorName;
+
+  $data['author_name'] = $disableauthorsAuthorName;
+
+	return $data;
+}
+
 add_filter('the_author', 'disableauthors_filter_author', 1);
 add_filter('get_the_author_display_name', 'disableauthors_filter_author', 1);
 add_filter('get_the_author_first_name', 'disableauthors_filter_author', 1);
 add_filter('get_the_author_last_name', 'disableauthors_filter_author', 1);
 add_filter('get_the_author_nickname', 'disableauthors_filter_author', 1);
 add_filter('get_the_author_user_login', 'disableauthors_filter_author', 1);
+
+
+add_filter('oembed_response_data', 'disableauthors_filter_oembed');
